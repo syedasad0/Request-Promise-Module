c61a7c122fce602d83e148d01880da4a82c4a457
SyedAsad <syedasad0@gmail.com>
2020-01-07 17:57:20 +0530

    DD1

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-const request = require('request-promise')
+const express = require('express')
+const rp = require('request-promise')
+const exphbs = require('express-handlebars')
+const path = require('path')
+
+const app = express()
+
+app.engine('.hbs', exphbs({
+  defaultLayout: 'main',
+  extname: '.hbs',
+  layoutsDir: path.join(__dirname, 'views/layouts')
+}))
+app.set('view engine', '.hbs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/:city', (req, res) => {
+  rp({
+    uri: 'http://apidev.accuweather.com/locations/v1/search',
+    qs: {
+      q: req.params.city,
+      apiKey: 'api-key'
+         // Use your accuweather API key here
+    },
+    json: true
+  })
+    .then((data) => {
+      res.render('index', data)
+    })
+    .catch((err) => {
+      console.log(err)
+      res.render('error')
+    })
+})
+
+app.listen(3000)
+console.log("Server Listening at: 3000");
